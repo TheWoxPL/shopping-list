e4e194fdf7da61ac3c87afcd0ab4c08fd8ff12f2
TheWoxPL <wojtek.bubula@onet.pl>
2025-01-13 15:07:01 +0100

    feat: create navbar component and homepage

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import styles from './Navbar.module.scss';
+import MenuSVG from '../../assets/svgs/menu.svg'
+import DotsVerticalSVG from '../../assets/svgs/dots-vertical.svg'
+
+export default function Navbar() {
+  return (
+    <>
+      <div className={styles.navbar}>
+        <img src={MenuSVG} className={styles.menuSVG} />
+        <div className={styles.title}>Lists</div>
+        <img src={DotsVerticalSVG} className={styles.options} />
+      </div>
+    </>
+  )
+}
