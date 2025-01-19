004419646d6129121d1ea787fe815b7df26cb22f
TheWoxPL <wojtek.bubula@onet.pl>
2025-01-19 18:58:24 +0100

    feat: change title

--- src/components/ChooseCategoryIcon/ChooseCategoryIcon.jsx
+++ src/components/ChooseCategoryIcon/ChooseCategoryIcon.jsx
@@ -1,23 +1,30 @@
-import styles from './ChooseCategoryIcon.module.scss';
-import PropTypes from 'prop-types';
+import styles from "./ChooseCategoryIcon.module.scss";
+import PropTypes from "prop-types";
 
-export default function ChooseCategoryIcon({ name, icon, handleCategoryClick, choosenCategory }) {
-
+export default function ChooseCategoryIcon({
+  name,
+  icon,
+  handleCategoryClick,
+  choosenCategory,
+}) {
   return (
-    <div 
-      className={choosenCategory === name ? styles.chooseCategoryIconClicked : styles.chooseCategoryIcon} 
+    <div
+      className={
+        choosenCategory === name
+          ? styles.chooseCategoryIconClicked
+          : styles.chooseCategoryIcon
+      }
       onClick={() => handleCategoryClick(name)}
     >
       <img src={icon} alt={name} />
       <span>{name}</span>
     </div>
-  )
+  );
 }
 
 ChooseCategoryIcon.propTypes = {
   name: PropTypes.string.isRequired,
   icon: PropTypes.string.isRequired,
   handleCategoryClick: PropTypes.func.isRequired,
-  choosenCategory: PropTypes.string.isRequired
+  choosenCategory: PropTypes.string.isRequired,
 };
-
